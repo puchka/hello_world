968f23fdaf2c5ec779796731538c74a574ad7f3e
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2024-05-02 10:14:33 +0400

    Expressions

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,11 @@
 use time;
 
-pub fn say_something(word: &str, t: Option<&time::Tm>) {
-    if let Some(t_ptr) = t {
-        println!("{}, world at {}!", word, t_ptr.asctime());
+pub fn say_something(word: &str, t: Option<&time::Tm>) -> time::Tm {
+    let t_val = if let Some(t_ptr) = t {
+        *t_ptr
     } else {
-        println!("{}, world at {}!", word, time::now_utc().asctime());
-    }
+        time::now_utc()
+    };
+    println!("{}, world at {}!", word, t_val.asctime());
+    t_val
 }
